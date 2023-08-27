1b5d504c41540c9f194f12f7d7e4b54ec4df98d8
Rohan Prabhu <rohan@commandk.dev>
2023-08-27 14:34:33 +0530

    Update cmdk-cli.rb

--- Formula/c/cmdk-cli.rb
+++ Formula/c/cmdk-cli.rb
@@ -5,26 +5,11 @@ class CmdkCli < Formula
     sha256 "9b2f344ea8729218f8428f07698b297f7c84117a201417bda92410ceadc94ce3"
     license ""
   
-    depends_on "curl"
-  
     def install
-      # ENV.deparallelize  # if your formula fails when building in parallel
-      # Remove unrecognized options if warned by configure
-      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
       bin.install "cmdk-osx-x86_64" => "cmdk"
-      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     end
   
     test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test cmdk-cli`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system "#{bin}/program", "do", "something"`.
-      system "false"
+      system "true"
     end
   end
