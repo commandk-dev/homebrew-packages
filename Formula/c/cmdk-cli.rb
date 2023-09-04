6fcdd89e03ff4d5741298f07f3bcad619498ebf2
Rohan Prabhu <rohan@commandk.dev>
2023-09-04 19:26:50 +0530

    Set latest release, 0.1.1 on brew

--- Formula/c/cmdk-cli.rb
+++ Formula/c/cmdk-cli.rb
@@ -1,8 +1,8 @@
 class CmdkCli < Formula
     desc "The CommandK CLI"
     homepage ""
-    url "https://github.com/rohanprabhu/cli-brew-test/releases/download/0.0.1/cmdk-cli-client-0.0.3-osx-x86_64.zip"
-    sha256 "9b2f344ea8729218f8428f07698b297f7c84117a201417bda92410ceadc94ce3"
+    url "https://github.com/commandk-dev/cli/releases/download/v0.1.1/cmdk-cli-client-0.1.1-osx-x86_64.zip"
+    sha256 "7578c463ff7d963ffa0b5622f2af01eb3e923a57a4f8b232c500d45226803348"
     license ""
   
     def install
