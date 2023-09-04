ec0fba0f0dc9c029c8964596b9fff11d018886ac
Rohan Prabhu <rohan@commandk.dev>
2023-09-04 19:34:21 +0530

    Specify version explicitly

--- Formula/c/cmdk-cli.rb
+++ Formula/c/cmdk-cli.rb
@@ -1,6 +1,7 @@
 class CmdkCli < Formula
     desc "The CommandK CLI"
     homepage ""
+    version "0.1.1"
     url "https://github.com/commandk-dev/cli/releases/download/v0.1.1/cmdk-cli-client-0.1.1-osx-x86_64.zip"
     sha256 "7578c463ff7d963ffa0b5622f2af01eb3e923a57a4f8b232c500d45226803348"
     license ""
